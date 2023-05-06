8d25fb6cf0683167c96843c25d1da0219c9f57c6
Lukas Licon <lukas@licons.com>
2023-05-05 20:12:03 -0700

    changed movements of bugs

--- game.js
+++ game.js
@@ -343,10 +343,6 @@ class Scene2 extends AdventureScene {
         )   
         this.addChemicals();
         this.addFlySwatter();
-        let score = this.add.text(this.w * .875 + this.s, this.h * .2)
-        .setText(elimCount)
-        .setStyle({ fontSize: `${3 * this.s}px` })
-        .setWordWrapWidth(this.w * 0.25 - 2 * this.s);
 
         let key = this.add.image(this.w * 0.45, this.w * 0.34, "keys")
             .setScale(.5)
@@ -959,9 +955,9 @@ const game = new Phaser.Game({
         width: 1920,
         height: 1080
     },
-    scene: [Intro, Scene1, Scene2, Scene3, Scene4, Outro1, Outro2],
+    //scene: [Intro, Scene1, Scene2, Scene3, Scene4, Outro1, Outro2],
     //1 scene: [Scene1, Intro, Scene2, Scene3, Scene4, Outro1, Outro2],
-    //2 scene: [Scene2, Intro, Scene1, Scene3, Scene4,  Outro1, Outro2],
+    scene: [Scene2, Intro, Scene1, Scene3, Scene4,  Outro1, Outro2],
     //scene: [Scene3, Intro, Scene1, Scene2, Scene4,  Outro1, Outro2],
     //scene: [Scene4, Intro, Scene1, Scene2, Scene3,  Outro1, Outro2],
     //scene: [ Outro1, Scene4, Intro, Scene1, Scene2, Scene3,  Outro2],
